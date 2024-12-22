9e7a484d93e83e15d64da9bae3a10ea914d9ad9f
Daniel Elde-Johansson <biz45dy2@students.codeinstitute.net>
2024-12-22 10:54:31 +0000

    Added welocme game with game name

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def welcome():
+    """
+    Add welcome page
+    Display game name and author
+    """
+print(" ")
+print(" ")
+print(" _____                                   _       ___ ___                                ")
+print("/  __ \                                 | |     |   ____|                   __     ")
+print("| /  \/  ___   _ __   _ __    ___   ___ | |_    |  |____    ___            |   \ ")
+print("| |     / _ \ |  _ \ |  _ \  / _ \ / __|| __|   |   ____|  / _ \  ||    || | () \    ")
+print("| \__/\| (_) || | | || | | ||  __/| (__ | |_    |  |      | (_) | ||    || |   _|       ")
+print(" \____/ \___/ |_| |_||_| |_| \___| \___| \__|   |__|       \___/  \\___//  |   \               ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+def main():
+    """
+    Run all program functions
+    """
+    welcome()
+if __name__ == "__main__":
+    main()
