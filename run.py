04a93029172803cb050a336b5a8b8d36695810d9
Daniel Elde-Johansson <biz45dy2@students.codeinstitute.net>
2024-12-22 11:17:41 +0000

    Added select game options

--- run.py
+++ run.py
@@ -17,15 +17,36 @@ print(" ")
 print(" ")
 print(" ")
 print(" ")
-def main():
-    """
-    Run all program functions
-    """
-    welcome()
-if __name__ == "__main__":
-    main()
 
 print ("Game Rules")
 print ("The objective of the game is to Connect Four tiles in the collums, they should either be connect vertically,diagonally or horizontally ") 
 print ("The collums will be numberd from 1 to 7, you will have to write the number of the collum you want to drop your piece. ")
 print("Good luck and enjoy!!!")  
+
+def select_game():
+    """
+    the program will first show two possible options
+    u can select a game to play versus a friend or the computer
+    """
+    print("Select game option:\n")
+    game_options = "1) 2 Players 2) Player vs. Computer\n"
+    game_selected = input(game_options)
+           
+    if game_selected == "1":
+        print("Player vs Player")
+    elif game_selected == "2":
+        print("Player vs computer")
+    else:
+        print("Not a valid option, please try again")
+        print("Select game option:\n")
+        game_selected = input(game_options)
+
+
+def main():
+    """
+    Run all program functions
+    """
+    welcome()
+    select_game()
+if __name__ == "__main__":
+    main()
